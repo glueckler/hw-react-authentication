91fc183e15c160554b2484406d2b94e97fc52f1f
Dean Glueckler <glueckler@gmail.com>
2018-10-23 19:25:27 -0700

    polished header nav

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,34 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, Fragment } from 'react';
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 class Header extends PureComponent {
   render() {
+    const authd = this.props.authenticated 
     return (
-      <div>
-        <Link to="/">App Home</Link>
-        <Link to="/signin">Sign In</Link>
-        <Link to="/signup">Sign Up</Link>
-        <Link to="/signout">Sign Out</Link>
-        <Link to="/feature">Secret Feature</Link>
+      <div style={{ display: 'flex', justifyContent: 'space-between', width: '450px' }}>
+          <Link to="/">App Home</Link>
+        {!authd && (
+          <Fragment>
+            <Link to="/signin">Sign In</Link>
+            <Link to="/signup">Sign Up</Link>
+          </Fragment>
+        )}
+        {authd && (
+          <Fragment>
+            <Link to="/feature">Secret Feature</Link>
+            <Link to="/signout">Sign Out</Link>
+          </Fragment>
+        )}
       </div>
     );
   }
 }
 
-export default Header
+const mapState = state => {
+  return {
+    authenticated: state.auth.authenticated,
+  }
+}
+
+export default connect(mapState)(Header)
