cca12dbc7785272fbc5941556b0c208579699db1
Dean Glueckler <glueckler@gmail.com>
2018-10-23 18:27:23 -0700

    redux with async request

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,6 +2,7 @@ import { AUTH_USER } from './types'
 import axios from 'axios'
 
 // redux-thunk allows up to pass functions into dispatch instead of objects
-export const signup = ({ email, password }) => dispatch => {
-  axios.post('http://localhost:3090/signup', { email, password })
+export const signup = ({ email, password }) => async dispatch => {
+  const response = await axios.post('http://localhost:3090/signup', { email, password })
+  dispatch ({ type: AUTH_USER, payload: response.data.token})
 }
